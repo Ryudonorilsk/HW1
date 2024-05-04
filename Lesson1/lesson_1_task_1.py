25ff2a624771c02d3d120ef02f12a08fd480ab71
Алексей <ya@alekseygavrilov.ru>
2024-05-04 17:01:22 +0300

    first homework

---
+++ Lesson1/lesson_1_task_1.py
@@ -0,0 +1,2 @@
+my_name = "Алексей"
+print ("Привет " + my_name)
---
+++ Lesson1/lesson_1_task_6.py
@@ -0,0 +1,32 @@
+def print_a():
+    print("1", end="")
+
+
+def print_b():
+    print("2", end="")
+
+def print_c():
+    print("3", end="")
+
+def print_d():
+    print("4", end="")
+
+def print_e():
+    print("5", end="")
+
+def print_f():
+    print("6", end="")
+
+def print_g():
+    print("7", end="")
+
+def print_h():
+    print("8", end="")
+
+def print_i():
+    print("9", end="")
+
+def print_j():
+    print("0", end="")
+
+print_h(), print_h(), print_j(), print_j(), print_e(), print_e(), print_e(), print_c(), print_e(), print_c(), print_e(), print(" проще позвонить чем у кого-то занимать!")
